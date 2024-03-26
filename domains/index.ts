bb53629869928a1aee4b563d9d2c9d45f30b4c39
zHatsuharu <zhatsuharu@gmail.com>
2024-03-26 10:41:10 +0100

    fix(routes): suppression domaines innexistants

--- domains/index.ts
+++ domains/index.ts
@@ -1,12 +1,8 @@
 import * as users from './users'
 import * as products from './products'
-import * as cereales from './cereales'
-import * as reparations from './reparations'
 
 export const domains = {
 
   'utilisateurs': users,
   products,
-  'kelloggs': cereales,
-  reparations,
 }
